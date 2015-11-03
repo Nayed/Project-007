7fb86f72994c5c75f6ac6302bc2be4803e09ad5e
jeanyves42 <jybruyas4265@gmail.com>
2015-11-03 08:53:26 +0000

    Migrations

--- database/migrations/2015_11_03_084709_create_lessons_table.php
+++ database/migrations/2015_11_03_084709_create_lessons_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateLessonsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('lessons', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('login')->unique();
-            $table->string('password', 60);
-            $table->integer('group_id')->unsigned()->index();
-            $table->rememberToken();
+            $table->string('slug');
+            $table->longtext('content');
+            $table->date('date');
+            $table->integer('category_id')->unsigned()->index();
+            $table->integer('user_id')->unsigned()->index();
             $table->timestamps();
         });
     }
@@ -30,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('lessons');
     }
 }
--- database/migrations/2015_11_03_085038_create_medias_table.php
+++ database/migrations/2015_11_03_085038_create_medias_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateMediasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('medias', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('login')->unique();
-            $table->string('password', 60);
-            $table->integer('group_id')->unsigned()->index();
-            $table->rememberToken();
+            $table->string('path');
+            $table->integer('lesson_id');
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('medias');
     }
 }
