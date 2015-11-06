5c40274d45b27b9fdbea706ea9dfa8635f57780c
lrocher_wq <lrocher@webqam.fr>
2015-11-06 11:58:51 +0100

    add note

--- resources/views/notes/add_note.blade.php
+++ resources/views/notes/add_note.blade.php
@@ -2,22 +2,24 @@
 @extends('app')
 
 @section('content')
-
-<p>Ajouter une note à : {{ $user->name }}  </p>
+<section id="add-note">
+ <p>Ajouter une note à : {{ $user->name }}  </p>
 
  {!! Form::open(array('url' => '/notes/update_note', 'method' => 'POST')) !!}
-                 
-{!! Form::hidden('id',$user->id);!!}
-{!! Form::hidden('id_lecon',$id_lecon);!!}
 
- {!! Form::label('note', 'Note');!!}
- {!! Form::text('note');!!}
-  </br>
+ {!! Form::hidden('id',$user->id);!!}
+ {!! Form::hidden('id_lecon',$id_lecon);!!}
+ <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-custom">
+  {!! Form::label('note', 'Note', array('class' => 'mdl-textfield__label');!!}
+  {!! Form::text('note', '', array('class' => 'mdl-textfield__input');!!}
+</div>
 
-  {!! Form::submit('Click Me!'); !!}
- 
+ {!! Form::submit('Click Me!', array('class' => 'mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised mdl-button--colored')); !!}
+
 
  {!! Form::close() !!}
+</section>
+
 
 
 @stop
--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -4,18 +4,24 @@
 @section('content')
 
 
-
+<section id="add-note-cours">
 
  {!! Form::open(array('url' => '/notes/redirect_note', 'method' => 'POST', 'files'=>true)) !!}
 
+ <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-custom">
 
-  {!! Form::label('cours', 'Liste de vos cours');!!}
-  {!! Form::select('lessons',  ($lessons)  ); !!}
+ {!! Form::label('cours', 'Liste de vos cours');!!}
+ {!! Form::select('lessons',  ($lessons)  ); !!}
+</div>
+ <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-custom">
 
-  {!! Form::submit('Click Me!'); !!}
- 
+ {!! Form::submit('Click Me!', array('class' => 'mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised mdl-button--colored')); !!}
+</div>
 
  {!! Form::close() !!}
+
+</section>
+
 
 
 
