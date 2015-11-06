1edf74f1fd0528db49b0195e623fe98291421dc1
lrocher_wq <lrocher@webqam.fr>
2015-11-06 12:21:35 +0100

    liste note

--- resources/views/notes/edit_note.blade.php
+++ resources/views/notes/edit_note.blade.php
@@ -2,22 +2,24 @@
 @extends('app')
 
 @section('content')
-
-<p>Ajouter une note à : {{ $user->name }}  </p>
+<section id="note-edit">
+ <p>Ajouter une note à : {{ $user->name }}  </p>
 
  {!! Form::open(array('url' => '/notes/update_note', 'method' => 'POST')) !!}
-                 
-{!! Form::hidden('id',$user->id);!!}
-{!! Form::hidden('id_lecon',$id_lecon);!!}
 
- {!! Form::label('note', 'Note');!!}
- {!! Form::text('note');!!}
-  </br>
-
-  {!! Form::submit('Click Me!'); !!}
- 
+ {!! Form::hidden('id',$user->id);!!}
+ {!! Form::hidden('id_lecon',$id_lecon);!!}
+ <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-custom">
+ {!! Form::label('note', 'Note', array('class' => 'mdl-textfield__label'));!!}
+ {!! Form::text('note', '' , array('class' => 'mdl-textfield__input'));!!}
+</div>
+ <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-custom">
+ {!! Form::submit('Click Me!', array('class' => 'mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised mdl-button--colored')); !!}
+</div>
 
  {!! Form::close() !!}
+</section>
+
 
 
 @stop
--- resources/views/notes/list_eleve.blade.php
+++ resources/views/notes/list_eleve.blade.php
@@ -1,65 +1,61 @@
 @extends('app')
 
 @section('content')
- @if (count($users) > 0)
-        <div class="panel panel-default">
-            
-            <div class="panel-heading">
-                Liste des eleves de la session
-            </div>
-           
-            <div class="panel-body">
-                <table class="table table-striped task-table">
+<section id="list-note">
+    @if (count($users) > 0)
 
-                    <!-- Table Headings -->
-                    <thead>
-                        <th>Prénom de l'éleve</th>
-                        <th>Note</th>
-                        <th>Actions</th>
-                        <th>&nbsp;</th>
-                    </thead>
+        <table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
 
-                    <!-- Table Body -->
-                    <tbody>
-                        @foreach ($users as $user)
+            <!-- Table Headings -->
+            <thead>
+            <th>Prénom de l'éleve</th>
+            <th>Note</th>
+            <th>Actions</th>
+            <th>&nbsp;</th>
+            </thead>
 
-                            <tr>
-                                <!-- Task Name -->
-                                <td class="table-text">
-                                    <div>{{ $user->name }}</div>
-                                </td>
-                                @unless ($user->notes->isEmpty())
-                                      <td class="table-text">
-                                        <div>  {{ ($user->notes[0]->value) }}</div>
-                                    </td>
-                                @endunless
-                                
-                               @unless (!$user->notes->isEmpty())
-                                    <td class="table-text">
-                                        <div>Non noté</div>
-                                    </td>
-                                @endunless
-                              
-                                    <td>
-                                        <form action="/notes/add_note/{{$id_lecon}}/{{ $user->id }}" method="GET">
-                                            {{ csrf_field() }}
-                                            <button>Ajouter une note</button>
-                                        </form>
-                                       
-                                        
-                                    </td>
-                                      
+            <!-- Table Body -->
+            <tbody>
+            @foreach ($users as $user)
 
-                                <td>
-                                    <!-- TODO: Delete Button -->
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </div>
+                <tr>
+                    <!-- Task Name -->
+                    <td class="table-text">
+                        <div>{{ $user->name }}</div>
+                    </td>
+                    @unless ($user->notes->isEmpty())
+                        <td class="table-text">
+                            <div>  {{ ($user->notes[0]->value) }}</div>
+                        </td>
+                    @endunless
+
+                    @unless (!$user->notes->isEmpty())
+                        <td class="table-text">
+                            <div>Non noté</div>
+                        </td>
+                    @endunless
+
+                    <td>
+                        <form action="/notes/add_note/{{$id_lecon}}/{{ $user->id }}" method="GET">
+                            {{ csrf_field() }}
+                            <button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised mdl-button--colored">Ajouter une note</button>
+                        </form>
+
+
+                    </td>
+
+
+                    <td>
+                        <!-- TODO: Delete Button -->
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+
     @endif
+</section>
+
 @endsection
 
 
