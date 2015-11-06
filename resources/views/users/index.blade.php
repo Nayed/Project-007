eb320d135a5966fbda6636fccee73f11653df431
Nayed SAID ALI <nayed.saidali@gmail.com>
2015-11-06 11:04:34 +0000

    view index user

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -44,43 +44,7 @@
      </table>
 
 
-<<<<<<< Updated upstream
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                Liste des users
-            </div>
 
-            <div class="panel-body">
-                <table class="table table-striped task-table">
-
-                    <!-- Table Headings -->
-                    <thead>
-                        <th>Name</th>
-                        <th>&nbsp;</th>
-                    </thead>
-
-                    <!-- Table Body -->
-                    <tbody>
-                        @foreach ($users as $user)
-                            <tr>
-                                <!-- Task Name -->
-                                <td class="table-text">
-                                    <div>{{ $user->name }}</div>
-                                    <form action="/user/edit/{{ $user->id }}" method="POST">
-                                            {{ csrf_field() }}
-
-                                            <button>Edit</button>
-                                    </form>
-                                </td>
-                            <tr> 
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </div>
-=======
-        
->>>>>>> Stashed changes
     @endif
      <button class="timeline-post-button mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab">
          <a class="add-user" href="/users/create" role="button">
